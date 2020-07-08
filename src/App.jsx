98527d312627e77b446050c9a0447ab3ef93e9dd
rmeizo <design@elenabaranova.com>
2020-07-08 09:36:53 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import {Header, Grid} from './components';
-import {MainPage, NewsPage, Members, Teachers, Profile} from './viwe';
+import {MainPage, NewsPage, Members, Teachers, Profile} from './pages';
 
 import './App.css';
 
@@ -11,15 +11,13 @@ function App() {
     <div>
       <Header />
       <Grid>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={()=>(<MainPage />)} />
-            <Route path="/news" component={()=>(<NewsPage />)} />
-            <Route path="/members" component={()=>(<Members />)} />
-            <Route path="/teachers" component={()=>(<Teachers />)} />
-            <Route path="/profile" component={()=>(<Profile />)} />
-          </Switch>
-        </Router>
+        <Switch>
+          <Route exact path="/" component={()=>(<MainPage />)} />
+          <Route path="/news" component={()=>(<NewsPage />)} />
+          <Route path="/members" component={()=>(<Members />)} />
+          <Route path="/teachers" component={()=>(<Teachers />)} />
+          <Route path="/profile" component={()=>(<Profile />)} />
+        </Switch>
       </Grid>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,5 +9,5 @@ export {
   NewsPage,
   Members,
   Teachers,
-  Profile
+  Profile,
 }
