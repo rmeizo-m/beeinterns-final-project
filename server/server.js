44b03b894e5d62a5a9164767346b58ba859138e6
rmeizo <design@elenabaranova.com>
2020-07-13 18:47:18 +0300

    upt-2-mini

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
-    const name = req.query.name || 'World';
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+ console.log("top");
 });
 
-app.listen(3001, () =>
-    console.log('Express server is running on localhost:3001')
-);
+app.listen(5000, () => console.log('Express server is running on localhost:5000'));
