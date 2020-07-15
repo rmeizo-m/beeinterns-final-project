edea0c7db9c6592b6248814eaf8b256af078afc5
Kate <ko-cat@yandex.ru>
2020-07-15 15:47:52 +0300

    Описание header, шрифт roboto, изменение фавиконки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import './header.css';
-
+import logo from '../../assets/header/logo1.png';
+import bar from '../../assets/header/hotdog.svg';
+// 
 const Header = () => {
 	return (
-		<header>
-		<div>Logo</div>
-		<nav className="">
-			<ul className="site-nav">
-				<li><a href="news">Новости</a></li>
-				<li><a href="members">Участники</a></li>
-				<li><a href="teachers">Преподователи</a></li>
-				<li><a href="profile">Профиль</a></li>
-			</ul>
-		</nav>
-	</header>
+	
+	// 	<header>
+	// 	<div>Logo</div>
+	// 	<nav className="">
+	// 		<ul className="site-nav">
+	// 			<li><a href="news">Новости</a></li>
+	// 			<li><a href="members">Участники</a></li>
+	// 			<li><a href="teachers">Преподователи</a></li>
+	// 			<li><a href="profile">Профиль</a></li>
+	// 		</ul>
+	// 	</nav>
+	// </header>
+
+	// <div class = "header">
+		<div class="header">
+            <div class="container">
+				
+					<div class="logo-wrap">
+						<img class="bar" src={bar} />
+						<img class="logo" src={logo} />
+					</div>
+
+					<div class="title-menu">
+						Beeinterns Level1
+					</div>
+
+					<ul class="header-menu">
+						<li><a class="header-menu-item" href="news"> Новости</a></li>
+						<li><a class="header-menu-item" href="members"> Участники</a></li>
+						<li><a class="header-menu-item" href="teachers"> Менторы</a></li>
+						<li><a class="header-menu-item" href="profile"> Профиль</a></li>
+					</ul>
+
+				
+            </div>
+		</div>
+        // </div>
+
+
 	);
 }
 export default Header;
+
+
