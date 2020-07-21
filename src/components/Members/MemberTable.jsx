fc26ffc0a9ee170aecdf88594ff00c46fa3da226
Kate <ko-cat@yandex.ru>
2020-07-21 12:31:04 +0300

    Добавлена область под фото участника

--- src/components/Members/MemberTable.jsx
+++ src/components/Members/MemberTable.jsx
@@ -5,21 +5,27 @@ import { fab, faTelegram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function MemberTable(){
     return <table class="members-table" >
+        <th class="empty-th"></th>
         <th>ФИО</th>
         <th class="second-th"><p>Git</p></th>
         <th class="second-th"><p>Mail</p></th>
         <th class="second-th"><p>Telegram</p></th>
-        <MembersTableRow username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
-        <MembersTableRow username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
-        <MembersTableRow username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
-        <MembersTableRow username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
-        <MembersTableRow username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
-        <MembersTableRow username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
+        <MembersTableRow  photoUrl="#" username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
+        <MembersTableRow  photoUrl="#" username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
+        <MembersTableRow  photoUrl="#" username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
+        <MembersTableRow  photoUrl="#" username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
+        <MembersTableRow  photoUrl="#" username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
+        <MembersTableRow  photoUrl="#" username="Екатарина Козловцева" gitlink="https://github.com/KateKozlovtseva" maillink="ea.kozlovtseva@physics.msu.ru" telink="@Ko-cat" ></MembersTableRow>
         </table>;
 }
 
 function MembersTableRow(props){
     return <tr class="member-row">
+    <td class="avatar-td">
+      <div class="member-avatar-wrapper">
+        <img class="member-avatar-image" src={props.photoUrl} alt=""/>
+      </div>
+    </td>
     <td>{props.username}</td>
     <td><a href="#" ><FontAwesomeIcon class="contacts-img" id="git" icon={faGithub} /><p class="member-link">{props.gitlink}</p></a></td>
     <td><a href="#" ><FontAwesomeIcon class="contacts-img" id="mail" icon={faEnvelope} /><p class="member-link">{props.maillink}</p></a></td>
