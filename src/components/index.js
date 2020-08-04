c707bb8baf93c34e92dd9b28a616b393b15f6626
rmeizo <design@elenabaranova.com>
2020-08-04 15:34:12 +0300

    Lesson

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from './Header/Header';
 import Grid from './Grid/Grid';
 import User from './User/User';
 import UserCard from './UserCard/UserCard';
+import  Lesson from './Lesson/Lesson';
 
 export {
   Header,
   Grid,
   User,
   UserCard,
+    Lesson,
 };
