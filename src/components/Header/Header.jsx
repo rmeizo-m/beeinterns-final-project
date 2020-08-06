3eedf2f226ffdc1d57006c50de4fa80a04772573
rmeizo <design@elenabaranova.com>
2020-08-06 18:51:03 +0300

    Добавил map и пример объекта с данными для компоненты lesson

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,11 @@ import bar from '../../assets/header/hotdog.svg';
 import cross from '../../assets/header/cross.svg';
 
 const menuItems = [
-  {link: "/", title: "Главная"},
-  {link: "/news", title: "Новости"},
-  {link: "/members", title: "Участники"},
-  {link: "/teachers", title: "Менторы"},
-  {link: "/profile", title: "Профиль"}
+  { link: '/', title: 'Главная' },
+  { link: '/news', title: 'Новости' },
+  { link: '/members', title: 'Участники' },
+  { link: '/teachers', title: 'Менторы' },
+  { link: '/profile', title: 'Профиль' },
 ];
 
 const Header = () => {
@@ -22,32 +22,32 @@ const Header = () => {
   }
 
   function MenuItem(item) {
-      const className = item.link == loc.pathname? "header-menu-selection":"header-menu-itom";
-      return <li class="header-menu-link"><a className= {className} active={item.link == loc.pat} href={item.link}>{item.title}</a></li>
+    const className = item.link === loc.pathname ? 'header-menu-selection' : 'header-menu-itom';
+    return <li className="header-menu-link"><a className={className} active={item.link === loc.pat} href={item.link}>{item.title}</a></li>;
   }
 
   return (
-  <div className="header">
-    <div className="container">
+    <div className="header">
+      <div className="container">
 
-      <div className="logo-wrap">
-        <a class= "bar-cross" onClick={toggleMenu}>
-          {menuVisible?
-            <img className="cross" src={cross} alt="cross" />:
-            <img className="bar" src={bar} alt="bar" />
-          }
-        </a>
-        <img className="logo" src={logo} alt="logo" />
-      </div>
+        <div className="logo-wrap">
+          <a className="bar-cross" onClick={toggleMenu}>
+            {menuVisible
+              ? <img className="cross" src={cross} alt="cross" />
+              : <img className="bar" src={bar} alt="bar" />}
+          </a>
+          <img className="logo" src={logo} alt="logo" />
+        </div>
 
-      <div className="title-menu">
-        {(menuItems.find(t => t.link == loc.pathname) || {title: 'Unknown page'}).title}
-      </div>
-      <ul className={menuVisible? "header-menu header-menu-open": "header-menu"}>
-        {menuItems.map(MenuItem)}
-      </ul>
+        <div className="title-menu">
+          {(menuItems.find((t) => t.link == loc.pathname) || { title: 'Unknown page' }).title}
+        </div>
+        <ul className={menuVisible ? 'header-menu header-menu-open' : 'header-menu'}>
+          {menuItems.map(MenuItem)}
+        </ul>
 
       </div>
-  </div>)
+    </div>
+  );
 };
 export default Header;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,38 +3,134 @@ import { User, Lesson, Select } from '../../components';
 
 import './MainPage.scss';
 
-export default class MainPage extends Component {
-  render() {
-    return (
-      <section className="inner">
-        <User />
-        <div className="lesson">
-          <div className="calendar">Calendar</div>
-          <div className="lesson-inner">
-            <form className="lesson-form" action="">
-              <Select/>
-              <Select/>
-            </form>
-            <div className="lesson-card">
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            </div>
+const MainPage = () => {
+  { /* Перенести в redux и json файл */ }
+  const lessonItem = [
+    {
+      id: 1, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 2, nameLesson: 'Введение в автотесты 2', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 3, nameLesson: 'Введение в автотесты 3', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 4, nameLesson: 'Введение в автотесты 4', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 5, nameLesson: 'Введение в автотесты 5', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 6, nameLesson: 'Введение в автотесты 6', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 7, nameLesson: 'Введение в автотесты 7', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 8, nameLesson: 'Введение в автотесты 8', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 9, nameLesson: 'Введение в автотесты 9', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 10, nameLesson: 'Введение в автотесты 10', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 11, nameLesson: 'Введение в автотесты 11', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 12, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 13, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 14, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 15, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 16, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 17, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 18, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 19, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 20, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 21, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 22, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 23, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 24, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 25, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 26, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 27, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 28, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 29, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 30, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 31, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 32, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 33, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 34, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+    {
+      id: 35, nameLesson: 'Введение в автотесты', mentor: 'Павел Уколов', date: '21.11.17',
+    },
+  ];
+  const lessons = lessonItem.map((lesson) => <Lesson key={lesson.id} id={lesson.id} nameLesson={lesson.nameLesson} mentor={lesson.mentor} date={lesson.date} />);
+
+  return (
+    <section className="inner">
+      <User />
+      <div className="lesson">
+        <div className="calendar">Calendar</div>
+        <div className="lesson-inner">
+          <form className="lesson-form" action="">
+            <Select />
+            <Select />
+          </form>
+          <div className="lesson-card">
+            {lessons}
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+export default MainPage;
